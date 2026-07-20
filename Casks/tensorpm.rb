fecc71f0170acdbe13d4eb7f3b0443b9bacd24ac
Simon <122058445+Neo552@users.noreply.github.com>
2026-07-20 09:34:04 +0200

    chore: bump TensorPM to 1.25.0

--- Casks/tensorpm.rb
+++ Casks/tensorpm.rb
@@ -1,6 +1,6 @@
 cask "tensorpm" do
-  version "1.24.0"
-  sha256 "3f3e16b080bb267ce29091754cae4cbe16a3c72ec6b690734daf8a052aa733a8"
+  version "1.25.0"
+  sha256 "cdf78388c3cf0939226ff4fa091ced9a5c55c1b8fc132ecb369c0b9626f542f6"
 
   url "https://github.com/Neo552/TensorPM-Releases/releases/download/v#{version}/TensorPM-macOS.dmg",
       verified: "github.com/Neo552/TensorPM-Releases/"
